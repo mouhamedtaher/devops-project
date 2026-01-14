0bf077e67f7037a694809b7a7dea8009c0e1eafa
Mouhamed Taher <medkahlaoui546@gmail.com>
2026-01-15 00:05:00 +0100

    Mise a jour v2.0.0

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def home():
     logger.info("Traitement de la page d'accueil")
     return jsonify({
         "message": "DevOps Project Final Version", 
-        "version": "2.0.0-LIVE-DEMO",
+        "version": "version": "2.0.0-FINAL",
         "trace_id": g.request_id
     })
 
